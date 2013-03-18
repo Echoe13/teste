34003aaf06c7a6cfd2a8b70810850c79f543bf68
Flr <flr124421@gmail.com>
2013-03-18 17:09:54 +0200

    Updatat cu schelet pentru aplicatie

--- gen/com/example/teste/R.java
+++ gen/com/example/teste/R.java
@@ -10,11 +10,40 @@ package com.example.teste;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int action_settings=0x7f080001;
+        public static final int edit_message=0x7f080000;
+    }
+    public static final class layout {
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int display_message=0x7f070000;
+        public static final int main=0x7f070001;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int action_settings=0x7f050006;
+        public static final int app_name=0x7f050000;
+        public static final int button_send=0x7f050002;
+        public static final int edit_message=0x7f050001;
+        public static final int hello_world=0x7f050007;
+        public static final int menu_settings=0x7f050003;
+        public static final int title_activity_display_message=0x7f050005;
+        public static final int title_activity_main=0x7f050004;
     }
     public static final class style {
         /** 
@@ -38,10 +67,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
